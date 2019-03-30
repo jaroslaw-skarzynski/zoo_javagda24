368fdff97070cabd570544926614b0989d10827f
rafal-rs <rafal-stefanski@wp.pl>
2019-03-30 15:32:09 +0100

    zwierzaki dodane

--- src/pl/sda/zoo/AnimalsFactory.java
+++ src/pl/sda/zoo/AnimalsFactory.java
@@ -12,6 +12,10 @@ public class AnimalsFactory {
 
         animals.add(new Mammal("Leo", "Whrrroaaa", "Lion"));
         animals.add(new Mammal("Jurek", "Whiii", "Elephant"));
+        animals.add(new Mammal("Tigru", "Wryy", "Tiger"));
+        animals.add(new Mammal("Wanda", "mute", "Fish"));
+        animals.add(new Mammal("Ptasiek", "ćwirćwir", "Bird"));
+
 
         return animals;
     }
