de9bee0cf9cf9f26f8e8f4ac3802e946f575eb99
MagdalenaNad <m_nadratowska@o2.pl>
2019-03-30 15:49:09 +0100

    new fishes

--- src/pl/sda/zoo/AnimalsFactory.java
+++ src/pl/sda/zoo/AnimalsFactory.java
@@ -1,6 +1,7 @@
 package pl.sda.zoo;
 
 import pl.sda.zoo.animals.Animal;
+import pl.sda.zoo.animals.Fish;
 import pl.sda.zoo.animals.Mammal;
 
 import java.util.ArrayList;
@@ -14,6 +15,8 @@ public class AnimalsFactory {
         animals.add(new Mammal("Jurek", "Whiii", "Elephant"));
         animals.add(new Mammal("Hania", "UuuuEE", "Hippo"));
         animals.add(new Mammal("Chian Ho", "Omnomnom", "Panda"));
+        animals.add(new Fish("Nemo", "bulbul", "Błazenek"));
+        animals.add(new Fish("Sebastian", "ruururu", "Dolphin"));
 
         return animals;
     }
